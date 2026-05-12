8efb5e09bff1dceecda104380f87d3369a772b7b
Jadi Khalid <jadikhalid@gmail.com>
2026-05-12 10:46:09 +0100

    working on the chat body layout...

---
+++ frontend/src/componets/MessageInput.jsx
@@ -0,0 +1,19 @@
+import React, { useRef, useState } from "react";
+import { useChatStore } from "../store/useChatStore";
+
+const MessageInput = () => {
+  const [text, setText] = useState("");
+  const [imagePreview, setImagePreview] = useState(null);
+  const fileInputRef = useRef(null);
+  const { sendMessage } = useChatStore();
+
+  const handleImageChange = (e) => {};
+
+  const removeImage = () => {};
+
+  const handleSendMessage = async (e) => {};
+
+  return <div>MessageInput</div>;
+};
+
+export default MessageInput;
